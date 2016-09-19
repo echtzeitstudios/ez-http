81880be0af929c0789f77b4a303c2361ba0fc099
Maximilian Haupt <mhaup@echtzeitstudios.com>
2016-09-19 22:53:45 +0200

    Add conan package description.

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import ConanFile, CMake
+from conans.tools import download, unzip, replace_in_file
+import os
+
+class EzHttpConan(ConanFile):
+    name = "ez-http"
+    version = "0.0.2"
+    url = "https://github.com/0x7f/ez-http"
+    license = "https://github.com/0x7f/ez-http/blob/master/LICENSE"
+    export = "*"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "OpenSSL/1.0.2h@lasote/stable", "Boost/1.60.0@lasote/stable", "http-parser/2.6.0@aptakhin/stable"
+    generators = "cmake"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=False", "fPIC=True"
+
+    def source(self):
+        zip_name = "v%s.zip" % self.version
+        download("https://github.com/0x7f/ez-http/archive/%s" % zip_name, zip_name)
+        unzip(zip_name)
+        os.unlink(zip_name)
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin") # From bin to bin
+        self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+
+    def build(self):
+        cmake = CMake(self.settings)
+        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
+        self.run('cmake --build . %s' % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="src")
+        self.copy("*.lib", dst="lib", src="lib")
+        self.copy("*.a", dst="lib", src="lib")
+
+    def package_info(self):
+        self.cpp_info.libs = ["ez-http"]
+
